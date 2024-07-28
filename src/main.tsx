9b9f263b74d538cb6b963f6d2579381b4b47a7b1
vaebe <18137693952@163.com>
2024-07-28 17:37:17 +0800

    style: 新增unocss依赖并更新相关配置

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import '@unocss/reset/normalize.css'
 import './index.css'
+import 'virtual:uno.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),UnoCSS()],
 })
