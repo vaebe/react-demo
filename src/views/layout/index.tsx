1b89e0a4b70c65ce9e2789acd9ff7a1579c9d81d
vaebe <18137693952@163.com>
2024-07-30 20:24:23 +0800

    feat(layout): 增加标题

--- src/views/layout/index.tsx
+++ src/views/layout/index.tsx
@@ -2,8 +2,11 @@ import { Outlet } from 'react-router-dom'
 
 export default function Layout() {
   return (
-    <div>
-      <p>admin</p>
+    <div className='h-screen w-screen bg-gray-100'>
+      <div className='h-[48px] flex items-center bg-white'>
+      <p className='text-2xl '>react demo</p>
+      </div>
+     
       <div>
         <Outlet />
       </div>
