016a52e2be1273dad6e152e862c2852c373418bd
vae <18137693952@163.com>
2024-10-12 17:02:33 +0800

    feat: 烟花效果

--- src/views/componentsView/index.tsx
+++ src/views/componentsView/index.tsx
@@ -1,9 +1,21 @@
 import { Message } from '@/components/message'
+import { Button } from 'antd'
+import confetti from 'canvas-confetti'
 
 export default function ComponentsView() {
+  function sendFireworks() {
+    confetti({
+      particleCount: 150,
+      spread: 60,
+      origin: { y: 0.2 },
+    })
+  }
+
   return (
     <div>
       <Message></Message>
+
+      <Button onClick={sendFireworks}>发射烟花</Button>
     </div>
   )
 }
